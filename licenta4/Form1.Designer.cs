6aac355a72023b337366047b76ae33c5aa45589f
vladic14 <136345362+vladic14@users.noreply.github.com>
2025-06-22 05:21:00 +0300

    Add files via upload

---
+++ licenta4/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace licenta4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.btnClear);
+            this.splitContainer1.Panel1.Controls.Add(this.btnDisplay);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.gMapControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(1107, 623);
+            this.splitContainer1.SplitterDistance = 368;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(85, 336);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(176, 58);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(85, 198);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(176, 58);
+            this.btnDisplay.TabIndex = 1;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(104, 122);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(16, 12);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(707, 599);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            this.gMapControl1.Load += new System.EventHandler(this.gMapControl1_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Alege Transportul";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 623);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Reateaua Transpoturilor Publice orasului Balti";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ licenta4/Form1.cs
@@ -0,0 +1,231 @@
+﻿using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace licenta4
+{
+    public partial class Form1 : Form
+    {
+        private GMapOverlay markersOverlay;
+        private Dictionary<string, List<BusStop>> busRoutes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            markersOverlay = new GMapOverlay("markers");
+            InitializeBusRoutes();
+
+            this.FormClosing += Form1_FormClosing; // Abonăm metoda de gestionare a închiderii formularului
+        }
+
+        private void InitializeBusRoutes()
+        {
+            // Initialize bus routes with sample data
+            busRoutes = new Dictionary<string, List<BusStop>>
+            {
+                {
+                    "Autobuzul 25", new List<BusStop>
+                    {
+                        new BusStop("Statia 1", new PointLatLng(47.788596, 27.896112)),
+                        new BusStop("Statia 2", new PointLatLng(47.783655, 27.894109)),
+                        new BusStop("Statia 3", new PointLatLng(47.782325, 27.890799)),
+                        new BusStop("Statia 4", new PointLatLng(47.778741, 27.892870)),
+                        new BusStop("Statia 5", new PointLatLng(47.773027, 27.898180)),
+                        new BusStop("Statia 6", new PointLatLng(47.768607, 27.902011)),
+                        new BusStop("Statia 7", new PointLatLng(47.766700, 27.903964)),
+                        new BusStop("Statia 8", new PointLatLng(47.766700, 27.903964)),
+                        new BusStop("Statia 9", new PointLatLng(47.760058, 27.917149)),
+                        new BusStop("Statia 10", new PointLatLng(47.759881, 27.922337)),
+                        new BusStop("Statia 11", new PointLatLng(47.761603, 27.925848)),
+                        new BusStop("Statia 12", new PointLatLng(47.763388, 27.929753)),
+                        new BusStop("Statia 13", new PointLatLng(47.766338, 27.936544)),
+                        new BusStop("Statia 14", new PointLatLng(47.767682, 27.939602))
+
+                    }
+                },
+                {
+                    "Troleibuzul 2", new List<BusStop>
+                    {
+                        new BusStop("Station A", new PointLatLng(47.783582, 27.901056)),
+                        new BusStop("Station B", new PointLatLng(47.783655, 27.894109)),
+                        new BusStop("Statia 3", new PointLatLng(47.782325, 27.890799)),
+                        new BusStop("Statia 4", new PointLatLng(47.778741, 27.892870)),
+                        new BusStop("Statia 5", new PointLatLng(47.773027, 27.898180)),
+                        new BusStop("Statia 6", new PointLatLng(47.768607, 27.902011)),
+                        new BusStop("Statia 7", new PointLatLng(47.766700, 27.903964)),
+                        new BusStop("Statia 8", new PointLatLng(47.766700, 27.903964)),
+                        new BusStop("Statia 9", new PointLatLng(47.760058, 27.917149)),
+                        new BusStop("Statia 10", new PointLatLng(47.759881, 27.922337)),
+                        new BusStop("Statia 11", new PointLatLng(47.761603, 27.925848)),
+                        new BusStop("Statia 12", new PointLatLng(47.763388, 27.929753)),
+                        new BusStop("Statia 13", new PointLatLng(47.766338, 27.936544)),
+                        new BusStop("Statia 14", new PointLatLng(47.767682, 27.939602))
+                    }
+                },
+
+                {
+                    "Troleibuzul 3", new List<BusStop>
+                    {
+                        new BusStop("Station A", new PointLatLng(47.766639, 27.904207)),
+                        new BusStop("Station B", new PointLatLng(47.764515, 27.898521)),
+                        new BusStop("Statia 3", new PointLatLng(47.762023, 27.894991)),
+                        new BusStop("Statia 4", new PointLatLng(47.757451, 27.887245)),
+                        new BusStop("Statia 5", new PointLatLng(47.754919, 27.883125)),
+                        new BusStop("Statia 6", new PointLatLng(47.753794, 27.881204)),
+                        new BusStop("Statia 8", new PointLatLng(47.762632, 27.911887)),
+                        new BusStop("Statia 9", new PointLatLng(47.760058, 27.917149)),
+                        new BusStop("Statia 10", new PointLatLng(47.759881, 27.922337)),
+                        new BusStop("Statia 11", new PointLatLng(47.761603, 27.925848)),
+                        new BusStop("Statia 12", new PointLatLng(47.763388, 27.929753)),
+                        new BusStop("Statia 13", new PointLatLng(47.766338, 27.936544)),
+                        new BusStop("Statia 14", new PointLatLng(47.767682, 27.939602))
+                    }
+                },
+
+
+                 {
+                    "Troleibuzul 6", new List<BusStop>
+                    {
+                        new BusStop("Station A", new PointLatLng(47.750403, 27.943925)),
+                        new BusStop("Station B", new PointLatLng(47.754493, 27.942047)),
+                        new BusStop("Statia 3", new PointLatLng(47.754364, 27.939328)),
+                        new BusStop("Statia 4", new PointLatLng(47.753069, 27.937273)),
+                        new BusStop("Statia 5", new PointLatLng(47.750843, 27.930745)),
+                        new BusStop("Statia 6", new PointLatLng(47.751240, 27.926818)),
+                        new BusStop("Statia 8", new PointLatLng(47.752279, 27.923063)),
+                        new BusStop("Statia 9", new PointLatLng(47.756060, 27.916870)),
+                        new BusStop("Statia 10", new PointLatLng(47.752836, 27.905368))
+                    }
+                 },
+
+                 {
+                    "Troleibuzul 8", new List<BusStop>
+                    {
+                        new BusStop("Station A", new PointLatLng(47.780772, 27.957811)),
+                        new BusStop("Station B", new PointLatLng(47.777961, 27.949635)),
+                        new BusStop("Statia 3", new PointLatLng(47.773901, 27.949539)),
+                        new BusStop("Statia 4", new PointLatLng(47.771082, 27.946352)),
+                        new BusStop("Statia 5", new PointLatLng(47.767952, 27.940124)),
+                        new BusStop("Statia 6", new PointLatLng(47.766878, 27.934578)),
+                        new BusStop("Statia 8", new PointLatLng(47.767938, 27.932142)),
+                        new BusStop("Statia 9", new PointLatLng(47.769397, 27.930218)),
+                        new BusStop("Statia 10", new PointLatLng(47.773572, 27.928630)),
+                        new BusStop("Statia 11", new PointLatLng(47.778078, 27.926731)),
+                        new BusStop("Statia 12", new PointLatLng(47.783269, 27.925551)),
+                        new BusStop("Statia 13", new PointLatLng(47.785410, 27.925181)),
+                        new BusStop("Statia 14", new PointLatLng(47.785410, 27.925181)),
+                        new BusStop("Statia 15", new PointLatLng(47.785410, 27.925181)),
+                        new BusStop("Statia 16", new PointLatLng(47.785410, 27.925181)),
+                        new BusStop("Statia 17", new PointLatLng(47.785410, 27.925181)),
+                        new BusStop("Statia 18", new PointLatLng(47.785410, 27.925181)),
+                        new BusStop("Statia 19", new PointLatLng(47.787176, 27.917284)),
+                        new BusStop("Statia 20", new PointLatLng(47.782728, 27.909710)),
+                        new BusStop("Statia 21", new PointLatLng(47.780933, 27.904678)),
+                        new BusStop("Statia 22", new PointLatLng(47.783698, 27.900933))
+                    }
+                }
+            };
+
+            comboBox1.Items.AddRange(busRoutes.Keys.ToArray());
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a bus route.");
+                return;
+            }
+
+            string selectedBus = comboBox1.SelectedItem.ToString();
+            if (busRoutes.ContainsKey(selectedBus))
+            {
+                DisplayRoute(busRoutes[selectedBus]);
+                CenterAndZoomRoute(busRoutes[selectedBus]);
+            }
+        }
+
+        private void DisplayRoute(List<BusStop> route)
+        {
+            markersOverlay.Markers.Clear();
+            foreach (var stop in route)
+            {
+                AddMarker(stop.Coordinates.Lat, stop.Coordinates.Lng, stop.Name);
+            }
+        }
+
+        private void AddMarker(double latitude, double longitude, string stopName)
+        {
+            GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(latitude, longitude), GMarkerGoogleType.red);
+            marker.ToolTipText = stopName; // Adăugăm denumirea stației ca text pentru tooltip
+            markersOverlay.Markers.Add(marker);
+            gMapControl1.Overlays.Add(markersOverlay);
+        }
+
+        private void CenterAndZoomRoute(List<BusStop> route)
+        {
+            if (route.Count > 0)
+            {
+                double avgLat = route.Average(p => p.Coordinates.Lat);
+                double avgLng = route.Average(p => p.Coordinates.Lng);
+                gMapControl1.Position = new PointLatLng(avgLat, avgLng);
+                gMapControl1.Zoom = 15; // Ajustăm nivelul de zoom după preferințe
+            }
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            gMapControl1.MinZoom = 2;
+            gMapControl1.MaxZoom = 18;
+            gMapControl1.Zoom = 15; // Setare zoom inițială pentru orașul Bălți
+            gMapControl1.Position = new GMap.NET.PointLatLng(47.7591, 27.9294); // Setare poziție inițială pentru Bălți, Republica Moldova
+            gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.ShowCenter = false;
+            gMapControl1.ShowTileGridLines = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Event handler for combobox selection change
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            markersOverlay.Markers.Clear();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Aici poți adăuga logica pentru a închide aplicația corect
+            // De exemplu, poți opri toate procesele și resursele utilizate de aplicație
+            // În cazul tău, opriți, probabil, conexiunea cu harta sau alte operațiuni necesare.
+            GMap.NET.GMaps.Instance.DisableTileHost(); // Oprește conexiunea cu serverul de hărți
+
+            // Poți adăuga alte acțiuni de închidere a aplicației aici, dacă este necesar
+
+            // Închiderea formularului va închide aplicația dacă acest formular este formularul principal
+        }
+
+    }
+
+    public class BusStop
+    {
+        public string Name { get; set; }
+        public PointLatLng Coordinates { get; set; }
+
+        public BusStop(string name, PointLatLng coordinates)
+        {
+            Name = name;
+            Coordinates = coordinates;
+        }
+    }
+}
+
